8112b7ec592311a4b49ab5799205e425a1b4f8ea
Temitope <adesiyantope2014@gmail.com>
2020-11-22 00:29:41 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+times = document.querySelector('.times');
+bars = document.querySelector('.bars');
+navlist = document.querySelector('.navlist')
+
+
+times.addEventListener('click', function(){
+    navlist.style.display = 'block';
+});
